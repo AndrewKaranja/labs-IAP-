bed6d58555612b561df351a6c1e6940227293110
AndrewKaranja <andywanji56@gmail.com>
2020-04-21 20:43:03 +0300

    error correction

--- DBConnector.php
+++ DBConnector.php
@@ -17,7 +17,7 @@ class DBConnector
 	
 	function __construct()
 	{
-		$conn=mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME) or die("Connection failed: ".mysqli_error());
+		$this->conn=mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME) or die("Connection failed: ".mysqli_error());
 
 		//mysql_select_db(DB_NAME,$this->conn);
 	}
